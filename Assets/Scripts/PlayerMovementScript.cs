256ec98990493843a5d3a72003dca5073e643ddb
Fluigip <fluigipanettieri@gmail.com>
2017-04-16 01:03:02 -0300

    Movement Player

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -7,19 +7,24 @@ public class PlayerMovementScript : MonoBehaviour {
     public float speed;
 
     private Transform playerPos;
+    private Rigidbody fplayer;
 
     public void Start()
     {
         playerPos = GetComponent<Transform>();
+        fplayer = GetComponent<Rigidbody>();
     }
 
     public void Update()
     {
+        
+    }
+
+    public void FixedUpdate()
+    {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        playerPos.position += movement * speed;
+        fplayer.MovePosition(movement * Time.fixedDeltaTime * speed + playerPos.position);
     }
 }
