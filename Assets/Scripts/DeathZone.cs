1c32dde8a3f4ff7ec04e08225031cc81da6b3aa1
Fluigip <fluigipanettieri@gmail.com>
2017-05-12 20:45:40 -0300

    Portal - WinBox - Enemy_AI

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+    public Transform spawnPoint;
+	// Use this for initialization
+	public void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+
+    
+
+    public void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.position = spawnPoint.position;
+    }
+}
---
+++ Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour {
+
+    Transform tr_Player;
+    float f_RootSpeed = 2.0f;
+    float f_MoveSpeed = 2.0f;
+	// Use this for initialization
+	void Start () {
+
+        tr_Player = GameObject.FindGameObjectWithTag("Player").transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*Look at player*/
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(tr_Player.position - transform.position), f_RootSpeed * Time.deltaTime);
+
+        /*Move at player*/
+        transform.position += transform.forward * f_MoveSpeed * Time.deltaTime;
+
+	}
+}
